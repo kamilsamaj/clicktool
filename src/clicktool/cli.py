7187756d4226af94ffbdb78a8bae2a0e082bb83e
Kamil Samaj <kamil.samaj@accolade.com>
2018-08-12 20:02:51 +0200

    ls command working

--- src/clicktool/cli.py
+++ src/clicktool/cli.py
@@ -5,5 +5,22 @@ Main entrypoint for the CLI
 import click
 
 
+@click.group(help='Clicktool dummmy CLI, it really does NOTHING')
+@click.version_option()
 def cli():
-    print('Hi there!')
+    """ Entry point to CLI options."""
+    pass  # do nothing here, it just defines the name for other subcommands
+
+
+@cli.command(name='ls', help='List, again dummy')
+@click.option('--all', '-a', is_flag=True)
+@click.option('--path', '-p', required=False)
+def ls(all, path):
+    """ This should do `ls`. """
+    to_echo = "ls"
+    if all:
+        to_echo = f'{to_echo} --all'
+    if path:
+        to_echo = f'{to_echo} --path {path}'
+
+    click.secho(to_echo)
